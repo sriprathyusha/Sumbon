b55edf2742badd0cb3497b91daf372657a6b78dd
Prathyusha Rani <prathyusharani@Prathyushas-MBP.hsd1.ca.comcast.net>
2017-02-21 17:03:16 -0800

    Working Copy

---
+++ www/js/controllers.js
@@ -0,0 +1,47 @@
+angular.module('starter.controllers', [])
+
+.controller('DashCtrl', function($scope) {})
+
+.controller('ChatsCtrl', function($scope, Chats) {
+  // With the new view caching in Ionic, Controllers are only called
+  // when they are recreated or on app start, instead of every page change.
+  // To listen for when this page is active (for example, to refresh data),
+  // listen for the $ionicView.enter event:
+  //
+  //$scope.$on('$ionicView.enter', function(e) {
+  //});
+
+  $scope.chats = Chats.all();
+  $scope.remove = function(chat) {
+    Chats.remove(chat);
+  };
+})
+
+.controller('sidemenu',function($scope,$ionicSideMenuDelegate){
+  $scope.toggleLeft=function(){
+    $ionicSideMenuDelegate.toggleLeft();
+  }
+})
+
+.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
+  $scope.chat = Chats.get($stateParams.chatId);
+})
+
+.controller('AccountCtrl', function($scope) {
+  $scope.settings = {
+    enableFriends: true
+  };
+})
+
+.controller('loginCtrl', function($scope) {
+})
+
+.controller('HomeCtrl',function($scope){
+
+});
+
+function MainCtrl($scope,$ionicSideMenuDelegate){
+  $scope.toggleRightSideMenu= function(){
+    $ionicSideMenuDelegate.toggleLeft();
+  };
+};
